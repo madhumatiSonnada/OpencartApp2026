106cc2579e89af8456c969c791a3ebfcb3ba47ea
Madhumati <madhumati217@gmail.com>
2026-02-20 20:07:27 +1100

    updated

---
+++ src/main/java/com/qa/Myapp/pages/cartpage.java
@@ -0,0 +1,13 @@
+package com.qa.Myapp.pages;
+
+public class cartpage 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is my cart page ");
+
+	}
+
+}
