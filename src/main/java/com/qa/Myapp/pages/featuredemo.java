a650701f25b4232e0183a1477ca3f86f3a0ce186
Madhumati <madhumati217@gmail.com>
2026-02-26 16:55:43 +1100

    checking git merge

---
+++ src/main/java/com/qa/Myapp/pages/featuredemo.java
@@ -0,0 +1,13 @@
+package com.qa.Myapp.pages;
+
+public class featuredemo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		String s="Checking on merge from master to local repo";
+		System.out.println(s);
+
+	}
+
+}
